509e0fe9f143005bb53059719575bd11b51d32f6
shiva <shivangishree020@gmail.com>
2025-08-24 19:16:40 +0530

    Replace process.env with import.meta.env

--- frontend/src/Utils/api.js
+++ frontend/src/Utils/api.js
@@ -1,7 +1,6 @@
-// /utils/api.js
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
 
 // Fetch all stories
 export const fetchStories = async () => {
