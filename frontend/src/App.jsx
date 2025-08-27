7546b9a23e4050709010d49f750b0940076020f4
shiva <shivangishree020@gmail.com>
2025-08-28 01:43:38 +0530

    Remove App.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Pages/Home/Home';
 import Account from './Components/Account/Account';
